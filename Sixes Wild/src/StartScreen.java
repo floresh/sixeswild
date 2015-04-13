f0bf78eaef8d73b3cc4a3d7cf7f42191a61eb81b
Kwame <ekampiah@wpi.edu>
2015-04-13 02:22:17 -0400

    minor updates

--- Sixes Wild/src/StartScreen.java
+++ Sixes Wild/src/StartScreen.java
@@ -1,61 +1,60 @@
 import javax.swing.JFrame;
 import javax.swing.JButton;
-import java.awt.BorderLayout;
-import javax.swing.GroupLayout;
-import javax.swing.GroupLayout.Alignment;
-import javax.swing.LayoutStyle.ComponentPlacement;
-import javax.swing.JTextField;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.SwingConstants;
+
+import net.miginfocom.swing.MigLayout;
+
 import java.awt.Font;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
-public class StartScreen extends JFrame{
-	private JTextField txtWildWhales;
-	public StartScreen() {
-	//None shall pass	
-		JButton btnNewButton_1 = new JButton("PLAY");
-		
-		txtWildWhales = new JTextField();
-		txtWildWhales.setFont(new Font("Gisha", Font.PLAIN, 30));
-		txtWildWhales.setHorizontalAlignment(SwingConstants.CENTER);
-		txtWildWhales.setText("Sixes Wild");
-		txtWildWhales.setColumns(10);
-		
-		JButton btnExit = new JButton("EXIT");
-		GroupLayout groupLayout = new GroupLayout(getContentPane());
-		groupLayout.setHorizontalGroup(
-			groupLayout.createParallelGroup(Alignment.LEADING)
-				.addGroup(groupLayout.createSequentialGroup()
-					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-						.addGroup(groupLayout.createSequentialGroup()
-							.addGap(71)
-							.addComponent(txtWildWhales, GroupLayout.PREFERRED_SIZE, 327, GroupLayout.PREFERRED_SIZE))
-						.addGroup(groupLayout.createSequentialGroup()
-							.addGap(184)
-							.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING, false)
-								.addComponent(btnExit, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-								.addComponent(btnNewButton_1, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 86, Short.MAX_VALUE))))
-					.addContainerGap(87, Short.MAX_VALUE))
-		);
-		groupLayout.setVerticalGroup(
-			groupLayout.createParallelGroup(Alignment.TRAILING)
-				.addGroup(groupLayout.createSequentialGroup()
-					.addGap(28)
-					.addComponent(txtWildWhales, GroupLayout.PREFERRED_SIZE, 93, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.RELATED, 153, Short.MAX_VALUE)
-					.addComponent(btnNewButton_1)
-					.addGap(18)
-					.addComponent(btnExit)
-					.addGap(85))
-		);
-		getContentPane().setLayout(groupLayout);
+public class StartScreen extends JFrame {
+	private JLabel titleLabel;
+	private JPanel panel;
+	private JButton exitButton;
+	private JButton playButton;
 
+	public StartScreen() {
+		super("Start Screen");
+		playButton = new JButton("PLAY");
+		playButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				new GameModeScreen();
+				dispose();
+			}
+		});
 		
-		setSize(400, 400);
+		exitButton = new JButton("EXIT");
+		exitButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.exit(EXIT_ON_CLOSE);
+			}
+		});
+
+		titleLabel = new JLabel("Sixes Wild");
+		titleLabel.setFont(new Font("Gisha", Font.PLAIN, 30));
+		titleLabel.setHorizontalAlignment(SwingConstants.CENTER);
+
+		panel = new JPanel();
+		panel.setLayout(new MigLayout("", "35%[center]35%",
+				"20%[]20%[]10%[]20%"));
+
+		panel.add(titleLabel, "span, wrap");
+		panel.add(playButton, "width 20%, wrap");
+		panel.add(exitButton, "width 20%");
+		
+		add(panel);
+
+		setSize(500, 450);
 		setLocationRelativeTo(null);
 		setVisible(true);
+		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
-	//Continue
+	// Continue
 }
