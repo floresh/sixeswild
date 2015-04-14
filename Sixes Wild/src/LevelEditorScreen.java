feb3d73c690a3d4df4cfef4a2310ef76b1597d06
Jake <Jake@Chairforce1>
2015-04-13 20:54:03 -0400

    Added tile frequencies, as well as options for setting maxmoves and maxtime.

--- Sixes Wild/src/LevelEditorScreen.java
+++ Sixes Wild/src/LevelEditorScreen.java
@@ -14,24 +14,107 @@ import java.awt.Component;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import javax.swing.SwingConstants;
+import javax.swing.JTextField;
+import javax.swing.JTextArea;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JScrollBar;
 
 
 public class LevelEditorScreen extends JFrame {
+	private JTextField textField;
+	private JTextField textField_1;
 	public LevelEditorScreen() {
+		
+		//comments
 		
 		JPanel panel = new JPanel();
 		panel.setBackground(Color.GREEN);
 		
-		//maybe it needs comments
-		//work
-		JPanel panel_1 = new JPanel();
+		textField = new JTextField();
+		textField.setText("0");
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setText("0");
+		textField_1.setColumns(10);
+		
+		JLabel lblNumberOfMoves = new JLabel("Number of Moves:");
+		lblNumberOfMoves.setToolTipText("Number of Moves");
+		
+		JLabel lblMaximumTime = new JLabel("Maximum Time:");
+		lblMaximumTime.setToolTipText("Maximum Time Limit");
+		
+		JLabel lblTileFrequency = new JLabel("Tile Frequency");
+		lblTileFrequency.setFont(new Font("Old English Text MT", Font.PLAIN, 21));
+		
+		JLabel label = new JLabel("1:");
+		
+		JLabel label_1 = new JLabel("2:");
+		
+		JLabel label_2 = new JLabel("3:");
+		
+		JLabel label_3 = new JLabel("4:");
+		
+		JLabel label_4 = new JLabel("5:");
+		
+		JLabel label_5 = new JLabel("6:");
+		
+		JScrollBar scrollBar = new JScrollBar();
+		scrollBar.setOrientation(JScrollBar.HORIZONTAL);
+		
+		JScrollBar scrollBar_1 = new JScrollBar();
+		scrollBar_1.setOrientation(JScrollBar.HORIZONTAL);
+		
+		JScrollBar scrollBar_2 = new JScrollBar();
+		scrollBar_2.setOrientation(JScrollBar.HORIZONTAL);
+		
+		JScrollBar scrollBar_3 = new JScrollBar();
+		scrollBar_3.setOrientation(JScrollBar.HORIZONTAL);
+		
+		JScrollBar scrollBar_4 = new JScrollBar();
+		scrollBar_4.setOrientation(JScrollBar.HORIZONTAL);
+		
+		JScrollBar scrollBar_5 = new JScrollBar();
+		scrollBar_5.setOrientation(JScrollBar.HORIZONTAL);
 		GroupLayout groupLayout = new GroupLayout(getContentPane());
 		groupLayout.setHorizontalGroup(
 			groupLayout.createParallelGroup(Alignment.TRAILING)
 				.addGroup(groupLayout.createSequentialGroup()
-					.addGap(51)
-					.addComponent(panel_1, GroupLayout.PREFERRED_SIZE, 171, GroupLayout.PREFERRED_SIZE)
-					.addGap(148)
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING, false)
+								.addGroup(groupLayout.createSequentialGroup()
+									.addGap(54)
+									.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
+										.addComponent(lblNumberOfMoves)
+										.addComponent(lblMaximumTime))
+									.addGap(18)
+									.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+										.addComponent(textField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+										.addComponent(textField_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)))
+								.addGroup(groupLayout.createSequentialGroup()
+									.addGap(105)
+									.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
+										.addComponent(label_1)
+										.addComponent(label)
+										.addComponent(label_5)
+										.addComponent(label_4)
+										.addComponent(label_3)
+										.addComponent(label_2))
+									.addPreferredGap(ComponentPlacement.RELATED)
+									.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+										.addComponent(scrollBar, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+										.addComponent(scrollBar_5, GroupLayout.PREFERRED_SIZE, 171, GroupLayout.PREFERRED_SIZE)
+										.addComponent(scrollBar_4, GroupLayout.PREFERRED_SIZE, 171, GroupLayout.PREFERRED_SIZE)
+										.addComponent(scrollBar_3, GroupLayout.PREFERRED_SIZE, 171, GroupLayout.PREFERRED_SIZE)
+										.addComponent(scrollBar_1, GroupLayout.PREFERRED_SIZE, 171, GroupLayout.PREFERRED_SIZE)
+										.addComponent(scrollBar_2, GroupLayout.PREFERRED_SIZE, 171, GroupLayout.PREFERRED_SIZE))))
+							.addPreferredGap(ComponentPlacement.RELATED, 77, Short.MAX_VALUE))
+						.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
+							.addContainerGap()
+							.addComponent(lblTileFrequency)
+							.addGap(112)))
 					.addComponent(panel, GroupLayout.PREFERRED_SIZE, 435, GroupLayout.PREFERRED_SIZE)
 					.addContainerGap())
 		);
@@ -42,9 +125,41 @@ public class LevelEditorScreen extends JFrame {
 					.addComponent(panel, GroupLayout.PREFERRED_SIZE, 378, GroupLayout.PREFERRED_SIZE)
 					.addContainerGap())
 				.addGroup(groupLayout.createSequentialGroup()
-					.addGap(89)
-					.addComponent(panel_1, GroupLayout.PREFERRED_SIZE, 225, GroupLayout.PREFERRED_SIZE)
-					.addContainerGap(117, Short.MAX_VALUE))
+					.addGap(94)
+					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+						.addComponent(textField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblNumberOfMoves))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+						.addComponent(textField_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblMaximumTime))
+					.addGap(24)
+					.addComponent(lblTileFrequency)
+					.addGap(18)
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addComponent(label)
+						.addComponent(scrollBar, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
+						.addComponent(scrollBar_1, GroupLayout.PREFERRED_SIZE, 21, GroupLayout.PREFERRED_SIZE)
+						.addComponent(label_1))
+					.addGap(18)
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addComponent(scrollBar_2, GroupLayout.PREFERRED_SIZE, 21, GroupLayout.PREFERRED_SIZE)
+						.addComponent(label_2))
+					.addGap(18)
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addComponent(scrollBar_3, GroupLayout.PREFERRED_SIZE, 21, GroupLayout.PREFERRED_SIZE)
+						.addComponent(label_3))
+					.addGap(18)
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addComponent(scrollBar_4, GroupLayout.PREFERRED_SIZE, 21, GroupLayout.PREFERRED_SIZE)
+						.addComponent(label_4))
+					.addGap(18)
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addComponent(scrollBar_5, GroupLayout.PREFERRED_SIZE, 21, GroupLayout.PREFERRED_SIZE)
+						.addComponent(label_5))
+					.addContainerGap(69, Short.MAX_VALUE))
 		);
 		getContentPane().setLayout(groupLayout);
 		
