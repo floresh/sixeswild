3b2d37fb154fae3704d22ac7899ae514a3bb6bf3
Jake <Jake@Chairforce1>
2015-04-14 21:42:21 -0400

    Splashes wild

---
+++ Sixes Wild/src/MainGame/SplashScreen.java
@@ -0,0 +1,55 @@
+package MainGame;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
+import java.awt.Image;
+
+import javax.swing.JLabel;
+
+import java.awt.BorderLayout;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.LayoutStyle.ComponentPlacement;
+
+//ok
+public class SplashScreen extends JFrame{
+	Image splash;
+	
+	public SplashScreen() {
+		setTitle("Sixes Wild Level Editor");
+		setSize(480, 480);
+		setLocationRelativeTo(null);
+		setVisible(true);
+		setResizable(false);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JLabel lblNewLabel = new JLabel("");
+		GroupLayout groupLayout = new GroupLayout(getContentPane());
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.TRAILING)
+				.addGroup(Alignment.LEADING, groupLayout.createSequentialGroup()
+					.addComponent(lblNewLabel)
+					.addContainerGap(450, Short.MAX_VALUE))
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addComponent(lblNewLabel)
+					.addContainerGap(408, Short.MAX_VALUE))
+		);
+		getContentPane().setLayout(groupLayout);
+		
+
+		lblNewLabel.setIcon(new ImageIcon("src\\images\\splash.jpg"));
+
+		
+		try{
+			Thread.sleep(1000);
+		}
+		catch(Exception e){
+			
+		}
+		dispose();
+		new StartScreen();
+	}
+}
