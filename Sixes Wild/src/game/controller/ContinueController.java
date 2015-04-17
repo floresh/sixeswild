b8c72fbb9db479454345acce1780c765a5896ed1
floresh <hericflores@gmail.com>
2015-04-17 18:58:59 -0400

    added empty Application and Model classes for controllers

---
+++ Sixes Wild/src/game/controller/ContinueController.java
@@ -0,0 +1,15 @@
+package game.controller;
+
+import MainGame.Application;
+import MainGame.Model;
+
+public class ContinueController {
+	Model model;
+	Application application;
+	
+	public ContinueController(Application app, Model m) {
+		this.model = m;
+		this.application = app;
+	}
+
+}
