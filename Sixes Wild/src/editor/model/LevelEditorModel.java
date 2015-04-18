09bf35fa3a1c67d7a1cbe2b3fc2da2dfb8bc4e51
Jake <Jake@Chairforce1>
2015-04-18 16:26:26 -0400

    added level to level editor model

--- Sixes Wild/src/editor/model/LevelEditorModel.java
+++ Sixes Wild/src/editor/model/LevelEditorModel.java
@@ -1,5 +1,20 @@
 package editor.model;
 
+import MainGame.Entities.Level;
+
 public class LevelEditorModel {
+	Level level;
+	
+	public LevelEditorModel(Level l){
+		this.level = l;
+	}
+	
+	Level getLevel(){
+		return this.level;
+	}
+	
+	void setLevel(Level l){
+		this.level = l;
+	}
 	
 }
