34f25535cc2d355d58abe6136e574d4c4d0cacd2
floresh <hericflores@gmail.com>
2015-04-22 15:33:03 -0400

    new main package

--- Sixes Wild/src/game/boundary/Application.java
+++ Sixes Wild/src/game/boundary/Application.java
@@ -1,6 +1,12 @@
 package game.boundary;
 
-public class Application {
+import game.entities.Model;
 
+public class Application {
+	Model model;
+	
+	public Application(Model model) {
+		this.model = model;
+	}
 	
 }
---
+++ Sixes Wild/src/game/main/Main.java
@@ -0,0 +1,26 @@
+package game.main;
+
+
+import game.boundary.SplashScreen;
+
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+public class Main {
+	public static void main(String[] args){
+		try {
+            for (UIManager.LookAndFeelInfo info : UIManager
+                    .getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException | 
+        		InstantiationException | 
+        		IllegalAccessException | 
+        		UnsupportedLookAndFeelException ex) {
+        }
+		new SplashScreen();
+	}
+}
