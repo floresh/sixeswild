ae863f455ff6fbdf1d02962dca754e65d1bd4768
Li Li <lli2@wpi.edu>
2015-04-23 18:44:28 -0400

    Board wiwth labels

--- Sixes Wild/src/game/boundary/BoardView.java
+++ Sixes Wild/src/game/boundary/BoardView.java
@@ -2,336 +2,41 @@ package game.boundary;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
 
 public class BoardView extends JPanel{
 	public BoardView() {
-		setLayout(null);
-		
-		JButton btnNewButton = new JButton("New button");
-		btnNewButton.setIcon(new ImageIcon(BoardView.class.getResource("/images/1man.png")));
-		btnNewButton.setBounds(30, 30, 50, 50);
-		add(btnNewButton);
-		
-		JButton btnNewButton_1 = new JButton("New button");
-		btnNewButton_1.setBounds(100, 30, 50, 50);
-		add(btnNewButton_1);
-		
-		JButton btnNewButton_2 = new JButton("New button");
-		btnNewButton_2.setBounds(170, 30, 50, 50);
-		add(btnNewButton_2);
-		
-		JButton btnNewButton_3 = new JButton("New button");
-		btnNewButton_3.setBounds(240, 30, 50, 50);
-		add(btnNewButton_3);
-		
-		JButton btnNewButton_4 = new JButton("New button");
-		btnNewButton_4.setBounds(310, 30, 50, 50);
-		add(btnNewButton_4);
-		
-		JButton btnNewButton_5 = new JButton("New button");
-		btnNewButton_5.setBounds(380, 30, 50, 50);
-		add(btnNewButton_5);
-		
-		JButton btnNewButton_6 = new JButton("New button");
-		btnNewButton_6.setBounds(450, 30, 50, 50);
-		add(btnNewButton_6);
-		
-		JButton btnNewButton_7 = new JButton("New button");
-		btnNewButton_7.setBounds(520, 30, 50, 50);
-		add(btnNewButton_7);
-		
-		JButton btnNewButton_8 = new JButton("New button");
-		btnNewButton_8.setBounds(590, 30, 50, 50);
-		add(btnNewButton_8);
-		
-		JButton button = new JButton("New button");
-		button.setBounds(30, 93, 50, 50);
-		add(button);
-		
-		JButton button_1 = new JButton("New button");
-		button_1.setBounds(100, 93, 50, 50);
-		add(button_1);
-		
-		JButton button_2 = new JButton("New button");
-		button_2.setBounds(30, 156, 50, 50);
-		add(button_2);
-		
-		JButton button_3 = new JButton("New button");
-		button_3.setBounds(30, 219, 50, 50);
-		add(button_3);
-		
-		JButton button_4 = new JButton("New button");
-		button_4.setBounds(30, 282, 50, 50);
-		add(button_4);
-		
-		JButton button_5 = new JButton("New button");
-		button_5.setBounds(30, 345, 50, 50);
-		add(button_5);
-		
-		JButton btnNewButton_9 = new JButton("New button");
-		btnNewButton_9.setBounds(30, 411, 50, 50);
-		add(btnNewButton_9);
-		
-		JButton btnNewButton_10 = new JButton("New button");
-		btnNewButton_10.setBounds(30, 482, 50, 50);
-		add(btnNewButton_10);
-		
-		JButton btnNewButton_11 = new JButton("New button");
-		btnNewButton_11.setBounds(30, 557, 50, 50);
-		add(btnNewButton_11);
-		
-		JButton button_6 = new JButton("New button");
-		button_6.setBounds(100, 156, 50, 50);
-		add(button_6);
-		
-		JButton button_7 = new JButton("New button");
-		button_7.setBounds(100, 219, 50, 50);
-		add(button_7);
-		
-		JButton button_8 = new JButton("New button");
-		button_8.setBounds(100, 282, 50, 50);
-		add(button_8);
-		
-		JButton button_9 = new JButton("New button");
-		button_9.setBounds(100, 345, 50, 50);
-		add(button_9);
-		
-		JButton button_10 = new JButton("New button");
-		button_10.setBounds(100, 411, 50, 50);
-		add(button_10);
-		
-		JButton button_11 = new JButton("New button");
-		button_11.setBounds(100, 482, 50, 50);
-		add(button_11);
-		
-		JButton button_12 = new JButton("New button");
-		button_12.setBounds(100, 557, 50, 50);
-		add(button_12);
-		
-		JButton button_13 = new JButton("New button");
-		button_13.setBounds(170, 93, 50, 50);
-		add(button_13);
-		
-		JButton button_14 = new JButton("New button");
-		button_14.setBounds(170, 156, 50, 50);
-		add(button_14);
-		
-		JButton button_15 = new JButton("New button");
-		button_15.setBounds(170, 219, 50, 50);
-		add(button_15);
-		
-		JButton button_16 = new JButton("New button");
-		button_16.setBounds(170, 282, 50, 50);
-		add(button_16);
-		
-		JButton button_17 = new JButton("New button");
-		button_17.setBounds(170, 345, 50, 50);
-		add(button_17);
-		
-		JButton button_18 = new JButton("New button");
-		button_18.setBounds(170, 411, 50, 50);
-		add(button_18);
-		
-		JButton button_19 = new JButton("New button");
-		button_19.setBounds(170, 482, 50, 50);
-		add(button_19);
-		
-		JButton button_20 = new JButton("New button");
-		button_20.setBounds(170, 557, 50, 50);
-		add(button_20);
-		
-		JButton button_21 = new JButton("New button");
-		button_21.setBounds(240, 93, 50, 50);
-		add(button_21);
-		
-		JButton button_22 = new JButton("New button");
-		button_22.setBounds(240, 156, 50, 50);
-		add(button_22);
-		
-		JButton button_23 = new JButton("New button");
-		button_23.setBounds(240, 219, 50, 50);
-		add(button_23);
-		
-		JButton button_24 = new JButton("New button");
-		button_24.setBounds(240, 282, 50, 50);
-		add(button_24);
-		
-		JButton button_25 = new JButton("New button");
-		button_25.setBounds(240, 345, 50, 50);
-		add(button_25);
-		
-		JButton button_26 = new JButton("New button");
-		button_26.setBounds(240, 411, 50, 50);
-		add(button_26);
-		
-		JButton button_27 = new JButton("New button");
-		button_27.setBounds(240, 482, 50, 50);
-		add(button_27);
-		
-		JButton button_28 = new JButton("New button");
-		button_28.setBounds(240, 557, 50, 50);
-		add(button_28);
-		
-		JButton button_29 = new JButton("New button");
-		button_29.setBounds(310, 93, 50, 50);
-		add(button_29);
-		
-		JButton button_30 = new JButton("New button");
-		button_30.setBounds(310, 156, 50, 50);
-		add(button_30);
-		
-		JButton button_31 = new JButton("New button");
-		button_31.setBounds(310, 219, 50, 50);
-		add(button_31);
-		
-		JButton button_32 = new JButton("New button");
-		button_32.setBounds(310, 282, 50, 50);
-		add(button_32);
-		
-		JButton button_33 = new JButton("New button");
-		button_33.setBounds(310, 345, 50, 50);
-		add(button_33);
-		
-		JButton button_34 = new JButton("New button");
-		button_34.setBounds(310, 411, 50, 50);
-		add(button_34);
-		
-		JButton button_35 = new JButton("New button");
-		button_35.setBounds(310, 482, 50, 50);
-		add(button_35);
-		
-		JButton button_36 = new JButton("New button");
-		button_36.setBounds(310, 557, 50, 50);
-		add(button_36);
-		
-		JButton button_37 = new JButton("New button");
-		button_37.setBounds(380, 93, 50, 50);
-		add(button_37);
-		
-		JButton button_38 = new JButton("New button");
-		button_38.setBounds(380, 156, 50, 50);
-		add(button_38);
-		
-		JButton button_39 = new JButton("New button");
-		button_39.setBounds(380, 219, 50, 50);
-		add(button_39);
-		
-		JButton button_40 = new JButton("New button");
-		button_40.setBounds(380, 282, 50, 50);
-		add(button_40);
-		
-		JButton button_41 = new JButton("New button");
-		button_41.setBounds(380, 345, 50, 50);
-		add(button_41);
-		
-		JButton button_42 = new JButton("New button");
-		button_42.setBounds(380, 411, 50, 50);
-		add(button_42);
-		
-		JButton button_43 = new JButton("New button");
-		button_43.setBounds(380, 482, 50, 50);
-		add(button_43);
-		
-		JButton button_44 = new JButton("New button");
-		button_44.setBounds(380, 557, 50, 50);
-		add(button_44);
-		
-		JButton button_45 = new JButton("New button");
-		button_45.setBounds(450, 93, 50, 50);
-		add(button_45);
-		
-		JButton button_46 = new JButton("New button");
-		button_46.setBounds(450, 156, 50, 50);
-		add(button_46);
-		
-		JButton button_47 = new JButton("New button");
-		button_47.setBounds(450, 219, 50, 50);
-		add(button_47);
-		
-		JButton button_48 = new JButton("New button");
-		button_48.setBounds(450, 282, 50, 50);
-		add(button_48);
-		
-		JButton button_49 = new JButton("New button");
-		button_49.setBounds(450, 345, 50, 50);
-		add(button_49);
-		
-		JButton button_50 = new JButton("New button");
-		button_50.setBounds(450, 411, 50, 50);
-		add(button_50);
-		
-		JButton button_51 = new JButton("New button");
-		button_51.setBounds(450, 482, 50, 50);
-		add(button_51);
-		
-		JButton button_52 = new JButton("New button");
-		button_52.setBounds(450, 557, 50, 50);
-		add(button_52);
-		
-		JButton button_53 = new JButton("New button");
-		button_53.setBounds(520, 93, 50, 50);
-		add(button_53);
-		
-		JButton button_54 = new JButton("New button");
-		button_54.setBounds(520, 156, 50, 50);
-		add(button_54);
-		
-		JButton button_55 = new JButton("New button");
-		button_55.setBounds(520, 219, 50, 50);
-		add(button_55);
-		
-		JButton button_56 = new JButton("New button");
-		button_56.setBounds(520, 282, 50, 50);
-		add(button_56);
-		
-		JButton button_57 = new JButton("New button");
-		button_57.setBounds(520, 345, 50, 50);
-		add(button_57);
-		
-		JButton button_58 = new JButton("New button");
-		button_58.setBounds(520, 411, 50, 50);
-		add(button_58);
-		
-		JButton button_59 = new JButton("New button");
-		button_59.setBounds(520, 482, 50, 50);
-		add(button_59);
-		
-		JButton button_60 = new JButton("New button");
-		button_60.setBounds(520, 557, 50, 50);
-		add(button_60);
-		
-		JButton button_61 = new JButton("New button");
-		button_61.setBounds(590, 93, 50, 50);
-		add(button_61);
-		
-		JButton button_62 = new JButton("New button");
-		button_62.setBounds(590, 156, 50, 50);
-		add(button_62);
-		
-		JButton button_63 = new JButton("New button");
-		button_63.setBounds(590, 219, 50, 50);
-		add(button_63);
-		
-		JButton button_64 = new JButton("New button");
-		button_64.setBounds(590, 282, 50, 50);
-		add(button_64);
-		
-		JButton button_65 = new JButton("New button");
-		button_65.setBounds(590, 345, 50, 50);
-		add(button_65);
-		
-		JButton button_66 = new JButton("New button");
-		button_66.setBounds(590, 411, 50, 50);
-		add(button_66);
-		
-		JButton button_67 = new JButton("New button");
-		button_67.setBounds(590, 482, 50, 50);
-		add(button_67);
-		
-		JButton button_68 = new JButton("New button");
-		button_68.setBounds(590, 557, 50, 50);
-		add(button_68);
-
+		GroupLayout groupLayout = new GroupLayout(this);
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGap(0, 600, Short.MAX_VALUE)
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGap(0, 600, Short.MAX_VALUE)
+		);
+		setLayout(groupLayout);
+		final JLabel[][] labelArr = new JLabel[9][9];
+		 
+		// JLabel label = new JLabel("New label");
+		// label.setBounds(30, 30, 50, 50);
+		// add(label);
+		for(int i = 0; i<=8;i++){
+			for(int j =0; j<=8;j++){
+				labelArr[i][j] = new JLabel("");
+				labelArr[i][j].setBounds(600,600,50,50);
+				labelArr[i][j].setBounds(30+(10*i)+(i*labelArr[i][j].getWidth()),30+(10*j)+(j*labelArr[i][j].getWidth()), 50,50);
+				add(labelArr[i][j]);
+			}
+	}
 	}
 }
