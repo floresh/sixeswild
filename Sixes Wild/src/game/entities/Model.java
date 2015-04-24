7b0646071a0114eadb519b98ee04db6d7c9d4a3d
Kwame <ekampiah@wpi.edu>
2015-04-23 22:26:27 -0400

    updates

--- Sixes Wild/src/game/entities/Model.java
+++ Sixes Wild/src/game/entities/Model.java
@@ -1,13 +1,7 @@
 package game.entities;
 
-import java.util.ArrayList;
-
-
-
 public class Model {
 	Level currentLevel;
-	ArrayList<Level> unlockedLevel = new ArrayList<Level>();
-	ArrayList<Level> highScores = new ArrayList<Level>();
 	int numSPSRemove;
 	int numSPSwap;
 	int numSPReshuffle;
