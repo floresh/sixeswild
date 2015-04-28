3bb5459484136ec0387ad7b21881f4a80d44a840
Jake <Jake@Chairforce1>
2015-04-27 20:01:12 -0400

    A couple small changes to leveleditor, added clear level and invert level

--- Sixes Wild/src/editor/controller/ClearLevelController.java
+++ Sixes Wild/src/editor/controller/ClearLevelController.java
@@ -1,19 +1,41 @@
 package editor.controller;
 
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+
 import editor.boundary.LevelEditorApplication;
+import editor.boundary.Main;
+import editor.boundary.WholesomeLevelEditorScreen;
 import game.entities.Model;
-public class ClearLevelController {
+public class ClearLevelController implements ActionListener {
 	
-	Model model;
-	LevelEditorApplication application;
+	WholesomeLevelEditorScreen wles;
 	
-	public ClearLevelController(LevelEditorApplication app, Model m) {
-		this.model = m;
-		this.application = app;
+	public ClearLevelController(WholesomeLevelEditorScreen wles) {
+		this.wles = wles;
 	}
 	
-	public boolean process(){
-		return false;
+	
+	public boolean setEnabled(boolean onOff, int row, int col){
+		Main.model.getCurrentLevel().getBoard().cells[row][col].setIsEnabled(onOff);
+		return true;
+	}
+	
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		for (int i = 0; i <= 8; i++) {
+			for (int j = 0; j <= 8; j++) {
+				if(wles.buttArray[i][j].getBackground() == Color.RED){
+					wles.buttArray[i][j].setBackground(Color.GREEN);
+					setEnabled(true,i,j);
+				}			
+			}
+		}
+		
 	}
 	
 }
