82e6b62dcb980b37e5ba8f5dcd276b98c4f4d8eb
Li Li <lli2@wpi.edu>
2015-05-04 20:29:59 -0400

    Current Test Coverage.

--- Sixes Wild/tests/ControllerTests.java
+++ Sixes Wild/tests/ControllerTests.java
@@ -1,29 +1,40 @@
-import java.awt.event.MouseEvent;
 
+import javax.swing.JFrame;
 import game.boundary.LevelView;
 import game.controller.SelectLevelController;
-import game.entities.PuzzleLevel;
+import game.entities.Board;
+import game.entities.Cell;
 import game.move.controller.MoveController;
+import game.move.controller.SelectionController;
 import junit.framework.TestCase;
 
 public class ControllerTests extends TestCase {
-	 
+	JFrame frame = new JFrame();
 	LevelView lv;
+	Cell[][] cells;
+	Cell cell;
 	
 	protected void setUp() {
-		PuzzleLevel pl = new PuzzleLevel();
-		LevelView lv = new LevelView(pl);
-		SelectLevelController slc = new SelectLevelController(lv, getName());
-		
+		Board b = new Board(cells);
+		for(int i = 0; i < 9; i++){
+			for(int j = 0; j < 9; j++){
+				cell = new Cell (i, j, true, false, false);
+			}
+		}
+		cell = new Cell (7, 8, true, false, false);
 	}
-	
-
 	protected void tearDown() {
+		lv.dispose();
+	}
+	
+	public void testSelect(){
+		SelectLevelController slc = new SelectLevelController(frame, "Puzzle");
 		
 	}
 	
-	public void testScoreController() {
-		
+	public void moveController() {
+		MoveController mc = new MoveController(lv);
+		SelectionController sc = new SelectionController(lv.getBoardView(), mc);
 	}
 
 }
